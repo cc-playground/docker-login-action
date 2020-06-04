fcadb95f68967be2e12f09c09088fbe3a4d0fa5d
Marco Spiess <marco.spiess@andrena.de>
2020-06-04 14:32:47 +0200

    fix cleanup script

--- src/cleanup.js
+++ src/cleanup.js
@@ -1,3 +1,10 @@
-const terminal = require('@actions/exec');
+(async () => {
+  const core = require('@actions/core');
+  const terminal = require('@actions/exec');
+  const loginServer = core.getInput('login-server', {required: true});
 
-terminal.exec('docker logout');
+  const exitCode = await terminal.exec(`docker logout ${loginServer}`);
+  if(exitCode !== 0) {
+    core.setFailed(`Exit code: ${exitCode}`);
+  }
+})();
