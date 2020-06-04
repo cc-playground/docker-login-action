e8d2fbd9e5265ab64f660b2ef0238d88dcb937b0
Marco Spiess <marco.spiess@andrena.de>
2020-06-04 14:12:33 +0200

    add cleanup script

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+const core = require('@actions/core');
 const terminal = require('@actions/exec');
 
-terminal.exec('docker ps');
+const username = core.getInput('username');
+const password = core.getInput('password');
+const loginServer = core.getInput('login-server');
+
+terminal.exec(`docker login -u ${username} -p ${password} ${loginServer}`);
